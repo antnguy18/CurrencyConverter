4f8d4bb3318072eaf7ac81f9304c329557a53a0b
Anthony Nguyen <nnguyen11910@gmail.com>
2024-04-21 18:41:22 -0400

    Initial commit

---
+++ currency-converter/src/main/java/currency/converter/App.java
@@ -0,0 +1,17 @@
+package currency.converter;
+
+import org.json.JSONObject;
+import javafx.animation.*;
+import javafx.application.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worldds!" );
+    }
+}
